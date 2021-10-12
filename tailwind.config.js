b4369a4c6c5f41c18bac2637e07d2d56482adfe6
Haris Chaniotakis <chanioxaris@gmail.com>
2021-10-12 12:14:19 +0300

    Initial commit
    Co-authored-by: Agis Anastasopoulos <agis@clerk.dev>
    Co-authored-by: Colin Sidoti <colin@clerk.dev>
    Co-authored-by: Peter Perlepes <p.perlepes@gmail.com>
    Co-authored-by: Nikos Douvlis <nikosdouvlis@gmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        indigo: {
+          100: '#ebeffe',
+          500: '#476BF2',
+          600: '#335bf1',
+          700: '#2e52d9',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
