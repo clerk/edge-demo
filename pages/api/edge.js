95ca8f633bc814e8876b50bf346a560130e2cbbe
Haris Chaniotakis <chanioxaris@gmail.com>
2021-10-13 14:27:20 +0300

    Testing

--- pages/api/edge.js
+++ pages/api/edge.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export function handler(req, res) {
+export default function handler(req, res) {
     res.status(200).send();
 }
 
