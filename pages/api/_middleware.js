67c55d9f13a29d0a0fc884878d0f9b409ce69acc
Haris Chaniotakis <chanioxaris@gmail.com>
2021-10-13 14:21:04 +0300

    Testing

--- pages/api/_middleware.js
+++ pages/api/_middleware.js
@@ -1,9 +1,18 @@
-import requireSession from '../../utils/middleware';
+// import requireSession from '../../utils/middleware';
+//
+// export default requireSession((req, res, next) => {
+//     if(req.path === '/api/edge') {
+//         res.send(req.session.userId);
+//         return
+//     }
+//     next();
+// })
 
-export default requireSession((req, res, next) => {
-    if(req.path === '/api/edge') {
-        res.send(req.session.userId);
-        return
-    }
+export default (req, res, next) => {
+    res.setHeader('x-edge', '1')
+    // if(req.path === '/api/edge') {
+    //     res.send(req.session.userId);
+    //     return
+    // }
     next();
-})
+}
