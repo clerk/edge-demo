d48a609add6c881510ce78e3c86d6ccf5bb6dfb4
Agis Anastasopoulos <agis@clerk.dev>
2021-10-13 16:02:03 +0300

    add kid

--- utils/middleware.js
+++ utils/middleware.js
@@ -61,6 +61,7 @@ async function verifyToken(token) {
             kty: 'RSA',
             key_ops: ['verify'],
             use: 'sig',
+            kid: 'ins_1zRcrQ8D4VOJvd9cxqoTW2iCTis',
             e: 'AQAB',
             n: "t3cmtkqQSWgUGiTz2j85WDHpozSHs9wAUEgzTnBQTEq7LBPQ_zSn4tHxI-7IO7J_EdjLI77dwh55DllEchY7boXR-nuuu56itC-pJq9GMrPrnFo_33cl0eJrEBXq1OBw65H0_GP4boHelKt1gJF9-kEiGE_En1CqYkso3-ARJZoRZwSty3pCe41iUJxzTaPnMbsUbGRQFDjAzfx3CpsPUloJr7-iscgkwZvkchc7b3DNyflOtVms20fNLN9COv0D4U7eu7ylmLKK5FZ5j05sySA9Ztsj3Vk5gKrKud1ESJ7dMYrOuKr5JPI_lhfO_NhajOUwXAex4YM6crJpyLNG7Q",
         }
