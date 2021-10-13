107d39d8fab82cb9eac2d32fd695c12b891a5e43
Haris Chaniotakis <chanioxaris@gmail.com>
2021-10-13 15:42:53 +0300

    Testing 6

--- utils/middleware.js
+++ utils/middleware.js
@@ -47,35 +47,54 @@ async function verifyToken(token) {
         return;
     }
     try {
-        const jwk = await retrieveJWK()
-        console.log('after retrieve jwk', jwk)
-        const pubKey = importJWK({
-            use: "sig",
-            kty: "RSA",
-            kid: "ins_1zRcrQ8D4VOJvd9cxqoTW2iCTis",
-            alg: "RS256",
+        // const jwk = await retrieveJWK()
+        // console.log('after retrieve jwk', jwk)
+
+        const tokenSplited = token.split('.')
+
+        const encoder = new TextEncoder();
+        const data = encoder.encode([tokenSplited[0], tokenSplited[1]].join('.'));
+        const signature = new Uint8Array(Array.from(tokenSplited[2]).map(c => c.charCodeAt(0)));
+
+        const jwk = {
+            alg: 'RS256',
+            kty: 'RSA',
+            key_ops: ['verify'],
+            use: 'sig',
+            e: 'AQAB',
             n: "t3cmtkqQSWgUGiTz2j85WDHpozSHs9wAUEgzTnBQTEq7LBPQ_zSn4tHxI-7IO7J_EdjLI77dwh55DllEchY7boXR-nuuu56itC-pJq9GMrPrnFo_33cl0eJrEBXq1OBw65H0_GP4boHelKt1gJF9-kEiGE_En1CqYkso3-ARJZoRZwSty3pCe41iUJxzTaPnMbsUbGRQFDjAzfx3CpsPUloJr7-iscgkwZvkchc7b3DNyflOtVms20fNLN9COv0D4U7eu7ylmLKK5FZ5j05sySA9Ztsj3Vk5gKrKud1ESJ7dMYrOuKr5JPI_lhfO_NhajOUwXAex4YM6crJpyLNG7Q",
-            e: "AQAB"
-        })
+        }
+       // const pubKey = await importJWK({
+       //      alg: 'RS256',
+       //      kty: 'RSA',
+       //      key_ops: ['verify'],
+       //      use: 'sig',
+       //      e: 'AQAB',
+       //      n: "t3cmtkqQSWgUGiTz2j85WDHpozSHs9wAUEgzTnBQTEq7LBPQ_zSn4tHxI-7IO7J_EdjLI77dwh55DllEchY7boXR-nuuu56itC-pJq9GMrPrnFo_33cl0eJrEBXq1OBw65H0_GP4boHelKt1gJF9-kEiGE_En1CqYkso3-ARJZoRZwSty3pCe41iUJxzTaPnMbsUbGRQFDjAzfx3CpsPUloJr7-iscgkwZvkchc7b3DNyflOtVms20fNLN9COv0D4U7eu7ylmLKK5FZ5j05sySA9Ztsj3Vk5gKrKud1ESJ7dMYrOuKr5JPI_lhfO_NhajOUwXAex4YM6crJpyLNG7Q",
+       //  })
+
+        const pubKey = await crypto.subtle.importKey('jwk', jwk, { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' }, true, ['verify']);
+
         console.log('after jose import jwk', pubKey)
 
         console.log(token)
 
-        // const ok = await crypto.subtle.verify('RSASSA-PKCS1-v1_5', process.env.CLERK_PUBLIC_KEY, token.split('.')[2], token.split('.')[1])
-        // console.log(ok)
-        // if (!ok) {
-        //     throw new Error('Not ok')
-        // }
-
-        const {payload} = await jwtVerify(token, pubKey, {algorithms: ['RS256']})
-
-        console.log('after verify', payload)
-
-        if (!payload.iss || !(payload.iss?.lastIndexOf('https://clerk.', 0) === 0)) {
-            throw new Error(`Invalid issuer: ${payload.iss}`)
+        const ok = await crypto.subtle.verify('RSASSA-PKCS1-v1_5', pubKey, signature, data)
+        // const ok = await crypto.subtle.verify('RSASSA-PKCS1-v1_5', pubKey, token.split('.')[2], token.split('.')[1])
+        console.log(ok)
+        if (!ok) {
+            throw new Error('Not ok')
         }
 
-        return payload
+        // const {payload} = await jwtVerify(token, pubKey, {algorithms: ['RS256']})
+
+        // console.log('after verify', payload)
+
+        // if (!payload.iss || !(payload.iss?.lastIndexOf('https://clerk.', 0) === 0)) {
+        //     throw new Error(`Invalid issuer: ${payload.iss}`)
+        // }
+
+        // return payload
     } catch (e) {
         console.log('verify token error', e)
         throw new Error(e)
