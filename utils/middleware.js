07306a1670ca7af33bcca0cd7917e19056278468
Haris Chaniotakis <chanioxaris@gmail.com>
2021-10-13 16:18:45 +0300

    Testing 8

--- utils/middleware.js
+++ utils/middleware.js
@@ -69,15 +69,15 @@ async function verifyToken(token) {
         //     n: "t3cmtkqQSWgUGiTz2j85WDHpozSHs9wAUEgzTnBQTEq7LBPQ_zSn4tHxI-7IO7J_EdjLI77dwh55DllEchY7boXR-nuuu56itC-pJq9GMrPrnFo_33cl0eJrEBXq1OBw65H0_GP4boHelKt1gJF9-kEiGE_En1CqYkso3-ARJZoRZwSty3pCe41iUJxzTaPnMbsUbGRQFDjAzfx3CpsPUloJr7-iscgkwZvkchc7b3DNyflOtVms20fNLN9COv0D4U7eu7ylmLKK5FZ5j05sySA9Ztsj3Vk5gKrKud1ESJ7dMYrOuKr5JPI_lhfO_NhajOUwXAex4YM6crJpyLNG7Q",
         // }
 
-       const pubKey = await importJWK({
-            alg: 'RS256',
-            kty: 'RSA',
-            key_ops: ['verify'],
-            use: 'sig',
-            kid: 'ins_1zRcrQ8D4VOJvd9cxqoTW2iCTis',
-            e: 'AQAB',
-            n: "t3cmtkqQSWgUGiTz2j85WDHpozSHs9wAUEgzTnBQTEq7LBPQ_zSn4tHxI-7IO7J_EdjLI77dwh55DllEchY7boXR-nuuu56itC-pJq9GMrPrnFo_33cl0eJrEBXq1OBw65H0_GP4boHelKt1gJF9-kEiGE_En1CqYkso3-ARJZoRZwSty3pCe41iUJxzTaPnMbsUbGRQFDjAzfx3CpsPUloJr7-iscgkwZvkchc7b3DNyflOtVms20fNLN9COv0D4U7eu7ylmLKK5FZ5j05sySA9Ztsj3Vk5gKrKud1ESJ7dMYrOuKr5JPI_lhfO_NhajOUwXAex4YM6crJpyLNG7Q",
-        })
+       // const pubKey = await importJWK({
+       //      alg: 'RS256',
+       //      kty: 'RSA',
+       //      key_ops: ['verify'],
+       //      use: 'sig',
+       //      kid: 'ins_1zRcrQ8D4VOJvd9cxqoTW2iCTis',
+       //      e: 'AQAB',
+       //      n: "t3cmtkqQSWgUGiTz2j85WDHpozSHs9wAUEgzTnBQTEq7LBPQ_zSn4tHxI-7IO7J_EdjLI77dwh55DllEchY7boXR-nuuu56itC-pJq9GMrPrnFo_33cl0eJrEBXq1OBw65H0_GP4boHelKt1gJF9-kEiGE_En1CqYkso3-ARJZoRZwSty3pCe41iUJxzTaPnMbsUbGRQFDjAzfx3CpsPUloJr7-iscgkwZvkchc7b3DNyflOtVms20fNLN9COv0D4U7eu7ylmLKK5FZ5j05sySA9Ztsj3Vk5gKrKud1ESJ7dMYrOuKr5JPI_lhfO_NhajOUwXAex4YM6crJpyLNG7Q",
+       //  })
 
         // const pubKey = await parseJwk(
         //     {
@@ -91,12 +91,13 @@ async function verifyToken(token) {
         //     'RS256'
         // );
 
-        // This hack should make jwtVerify work
-        //globalThis.CryptoKey = pubKey.constructor;
-
-        // const pubKey = await crypto.subtle.importKey('jwk', jwk, { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' }, true, ['verify']);
+        // const pubKey = await crypto.subtle.importKey('spki', jwk, { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' }, true, ['verify']);
+        const pubKey = await crypto.subtle.importKey('spki', process.env.CLERK_PUBLIC_KEY, { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' }, true, ['verify']);
 
         // console.log('after jose import jwk', pubKey)
+
+        // This hack should make jwtVerify work
+        // globalThis.CryptoKey = pubKey.constructor;
 
         // const ok = await crypto.subtle.verify('RSASSA-PKCS1-v1_5', pubKey, signature, data)
         // // const ok = await crypto.subtle.verify('RSASSA-PKCS1-v1_5', pubKey, token.split('.')[2], token.split('.')[1])
