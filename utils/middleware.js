445f97825c5ce945df1d4070339865e3de325b07
Agis Anastasopoulos <agis@clerk.dev>
2021-10-13 20:59:42 +0300

    Respond with 401

--- utils/middleware.js
+++ utils/middleware.js
@@ -33,7 +33,7 @@ export default function requireSession(handler) {
             handler(req, res, next)
         } catch (error) {
             console.log('Error: ', error)
-            res.status(500).send(error)
+            res.status(401).send(error)
         }
     }
 }
