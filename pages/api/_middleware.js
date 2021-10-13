be0747bfbd0712acc54de155e940ddc156ce21cf
Haris Chaniotakis <chanioxaris@gmail.com>
2021-10-13 14:37:30 +0300

    Testing

--- pages/api/_middleware.js
+++ pages/api/_middleware.js
@@ -1,9 +1,5 @@
 import requireSession from '../../utils/middleware';
 
 export default requireSession((req, res, next) => {
-    // if(req.path === '/api/edge') {
         res.send(req.session.userId);
-        // return
-    // }
-    // next();
 })
