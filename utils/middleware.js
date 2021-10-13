04fc3e5dff0d7c2cdb3af3ec9140fcec440b0e75
Haris Chaniotakis <chanioxaris@gmail.com>
2021-10-13 15:30:23 +0300

    Testing 4

--- utils/middleware.js
+++ utils/middleware.js
@@ -49,7 +49,14 @@ async function verifyToken(token) {
     try {
         const jwk = await retrieveJWK()
         console.log('after retrieve jwk', jwk)
-        const pubKey = importJWK(jwk)
+        const pubKey = importJWK({
+            use: "sig",
+            kty: "RSA",
+            kid: "ins_1zRcrQ8D4VOJvd9cxqoTW2iCTis",
+            alg: "RS256",
+            n: "t3cmtkqQSWgUGiTz2j85WDHpozSHs9wAUEgzTnBQTEq7LBPQ_zSn4tHxI-7IO7J_EdjLI77dwh55DllEchY7boXR-nuuu56itC-pJq9GMrPrnFo_33cl0eJrEBXq1OBw65H0_GP4boHelKt1gJF9-kEiGE_En1CqYkso3-ARJZoRZwSty3pCe41iUJxzTaPnMbsUbGRQFDjAzfx3CpsPUloJr7-iscgkwZvkchc7b3DNyflOtVms20fNLN9COv0D4U7eu7ylmLKK5FZ5j05sySA9Ztsj3Vk5gKrKud1ESJ7dMYrOuKr5JPI_lhfO_NhajOUwXAex4YM6crJpyLNG7Q",
+            e: "AQAB"
+        })
         console.log('after jose import jwk', pubKey)
 
         console.log(token)
