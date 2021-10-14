5aa7b756db1e7cb0546374b3aa28bc024bb5dcd3
Haris Chaniotakis <chanioxaris@gmail.com>
2021-10-14 11:30:04 +0300

    Render response time

--- pages/regular.js
+++ pages/regular.js
@@ -3,15 +3,19 @@ import React, {useState} from 'react';
 export default function Regular() {
     const [statusCode, setStatusCode] = useState(0)
     const [data, setData] = useState('')
+    const [responseTime, setResponseTime] = useState(0)
 
     const APIEndpoint = '/api/hello'
 
     React.useEffect(async () => {
         const retrieveData = async () => {
+            const start = new Date().getTime();
+
             const response = await fetch(APIEndpoint, {
                 method: 'GET',
             })
 
+            setResponseTime((new Date().getTime()) - start)
             setStatusCode(response.status)
 
             if (response.status === 200) {
@@ -29,12 +33,16 @@ export default function Regular() {
     return (
         <>
             <div style={{padding: '15px', margin: '15px'}}>
-                Retrieved session from regular endpoint with status code
-                status code: &nbsp;<span style={{fontWeight: 'bold', color: statusCode === 200 ? 'green' : 'red'}}> {statusCode} </span>
+                Retrieved session from regular endpoint with status code:
+                &nbsp;<span style={{fontWeight: 'bold', color: statusCode === 200 ? 'green' : 'red'}}> {statusCode} </span>
 
                 <br /><br />
 
-                claims from the verified token:
+                Response time: {responseTime} ms
+
+                <br /><br />
+
+                Claims from the verified token:
                 <br />
                 <pre>{data}</pre>
             </div>
