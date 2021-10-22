b40775fecd45bd8ce786fd256447166c262d378d
colinclerk <colin@clerk.dev>
2021-10-22 13:25:38 -0400

    Revert because it didn't work on prod

--- pages/api/_middleware.js
+++ pages/api/_middleware.js
@@ -9,30 +9,30 @@
 import { NextResponse } from "next/server";
 
 export default function authTimer(evt) {
-  const withAuthSpeed = async function () {
-    // If the timer is started, proceed to NextRequest.next();
-    if (evt.request.headers.get("X-Timer")) {
-      return NextResponse.next();
-    }
+  // const withAuthSpeed = async function () {
+  //   // If the timer is started, proceed to NextRequest.next();
+  //   if (evt.request.headers.get("X-Timer")) {
+  //     return NextResponse.next();
+  //   }
 
-    // No timer set, set one and re-request via fetch()
-    evt.request.headers.set("X-Timer", "1");
-    const timerStart = new Date().getTime();
-    const res = await fetch(
-      new URL(evt.request.nextUrl, evt.request.headers.get("referer")),
-      {
-        headers: evt.request.headers,
-      }
-    );
-    const timerEnd = new Date().getTime();
-    const data = await res.json();
+  //   // No timer set, set one and re-request via fetch()
+  //   evt.request.headers.set("X-Timer", "1");
+  //   const timerStart = new Date().getTime();
+  //   const res = await fetch(
+  //     new URL(evt.request.nextUrl, evt.request.headers.get("referer")),
+  //     {
+  //       headers: evt.request.headers,
+  //     }
+  //   );
+  //   const timerEnd = new Date().getTime();
+  //   const data = await res.json();
 
-    return new Response(
-      JSON.stringify({
-        ...data,
-        authenticationTime: timerEnd - timerStart,
-      })
-    );
-  };
-  evt.respondWith(withAuthSpeed());
+  //   return new Response(
+  //     JSON.stringify({
+  //       ...data,
+  //       authenticationTime: timerEnd - timerStart,
+  //     })
+  //   );
+  // };
+  evt.respondWith(NextResponse.next());
 }
