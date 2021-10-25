81b43544613ed0a451b601e29f231dd580491130
colinclerk <colin@clerk.dev>
2021-10-25 02:25:50 -0400

    fix tailwind config for prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 module.exports = {
-  purge: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./page-sections/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         indigo: {
-          100: "#ebeffe",
-          500: "#476BF2",
-          600: "#335bf1",
-          700: "#2e52d9",
+          100: '#ebeffe',
+          500: '#476BF2',
+          600: '#335bf1',
+          700: '#2e52d9',
         },
       },
     },
