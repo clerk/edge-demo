60a7e456867e2afb04beaa26348f690eee60421c
colinclerk <colin@clerk.dev>
2021-10-25 03:23:14 -0400

    try again

--- pages/api/stateful/index.js
+++ pages/api/stateful/index.js
@@ -1,4 +1,4 @@
 // Production requires something to be here or the request 404s
-export default function stateful(req, res) => {
+export default function stateful(req, res) {
   res.send('');
-};
+}
--- pages/api/stateless/index.js
+++ pages/api/stateless/index.js
@@ -1,4 +1,4 @@
 // Production requires something to be here or the request 404s
-export default function stateless(req, res) => {
+export default function stateless(req, res) {
   res.send('');
-};
+}
