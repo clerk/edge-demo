0c3dadc81ca9b366ab43a485867d299f0f2a2a2f
colinclerk <colin@clerk.dev>
2021-10-25 02:37:21 -0400

    Add utils to postcss

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import {
   HeroSection,
   Layout,
@@ -9,6 +10,10 @@ import {
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>Authentication at the edge | Clerk x Next.js</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <HeroSection />
       <TwoStrategiesSection />
       <StatelessDoneRightSection />
