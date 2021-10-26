9ff7768993dba4ed0fbe634a828bf8b3a929c2ae
Peter Perlepes <p.perlepes@gmail.com>
2021-10-26 13:45:47 +0300

    fix: React builder failing prerendering

--- components/GoogleTagManager.jsx
+++ components/GoogleTagManager.jsx
@@ -1,17 +1,15 @@
 const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
 
 export function GoogleTagManager() {
-  return (
-    GTM_ID && (
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  return GTM_ID ? (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','${GTM_ID}');`,
-        }}
-      ></script>
-    )
-  );
+      }}
+    ></script>
+  ) : null;
 }
--- components/SentryLoader.jsx
+++ components/SentryLoader.jsx
@@ -1,10 +1,10 @@
 const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 export const SentryLoader = () =>
-  SENTRY_DSN && (
+  SENTRY_DSN ? (
     // eslint-disable-next-line @next/next/no-sync-scripts
     <script
       src={`https://js.sentry-cdn.com/${SENTRY_DSN}.min.js`}
       crossOrigin='anonymous'
     />
-  );
+  ) : null;
